2fba2d8a7709af622b2f2f35c1ca980180fa9a94
Tomas Bjurman <tbjurman@gmail.com>
2021-03-01 17:03:07 +0100

    add patch: dwm-scratchpad-6.2.diff

--- config.def.h
+++ config.def.h
@@ -62,6 +62,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *mutecmd[] = { "volume.sh", "toggle", NULL };
 static const char *volupcmd[] = { "volume.sh", "5%+", "unmute", NULL };
 static const char *voldowncmd[] = { "volume.sh", "5%-", "unmute", NULL };
@@ -72,6 +74,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
